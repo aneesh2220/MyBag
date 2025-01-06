d0cc82eb0efe10b611cf1c3b1ccab8e2fd11d950
aneesh2220 <tiwarianeesh629@gmail.com>
2025-01-06 12:37:36 +0530

    delete_func

--- src/components/modal.jsx
+++ src/components/modal.jsx
@@ -1,7 +1,36 @@
+import { useState } from "react";
 import "../styles/modal.css";
 import { ImCross } from "react-icons/im";
+import { deleteDoc, doc, updateDoc } from "firebase/firestore";
+import { dataBase } from "../firebase.js";
 
 let ProductModal = ({ exit, val }) => {
+  let [quantity, setQuantity] = useState(val.quantity);
+  let [status, setStatus] = useState(val.status);
+
+  let handleSave = async (e) => {
+    e.preventDefault();
+    try {
+      let docRef = doc(dataBase, "products", val.id);
+      await updateDoc(docRef, { quantity: quantity });
+      exit();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  let handleDelete = async (e) => {
+    
+
+    try {
+      let docRef = doc(dataBase, "products", val.id);
+      await deleteDoc(doc(dataBase, "products", val.id));
+      exit();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div className="product-modal">
       <div className="exit-payments" onClick={exit}>
@@ -9,11 +38,39 @@ let ProductModal = ({ exit, val }) => {
       </div>
 
       <div className="content">
-        <p>{val.name}</p>
-        <p>{val.categry}</p>
-        <p>{val.type}</p>
-        <p>{val.quantity}</p>
-        <p>{val.status}</p>
+        <h2>Basic Details <span><button onClick={handleDelete}>Delete</button></span></h2>
+        <p>
+          <span>Name:</span>
+          {val.name}
+        </p>
+        <p>
+          <span>Category:</span>
+          {val.categry}
+        </p>
+        <p>
+          <span>Type:</span>
+          {val.type}
+        </p>
+        <div>
+          <div></div>
+
+          <form onSubmit={handleSave}>
+            <div className="editable">
+              <h2>Quantity and Status</h2>
+              <button type="submit">Save</button>
+            </div>
+            <div className="editable">
+              <h3>Quantity:</h3>
+              <input
+                type="number"
+                value={quantity}
+                onChange={(e) => {
+                  setQuantity(e.target.value);
+                }}
+              />
+            </div>
+          </form>
+        </div>
       </div>
     </div>
   );
